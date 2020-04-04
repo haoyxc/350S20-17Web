9f221f242a2ff17f9ffb305f885570e66c3560a5
haoyxc <48307028+haoyxc@users.noreply.github.com>
2020-04-04 16:56:04 -0500

    initial

---
+++ src/components/POIDisplay.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class POIDisplay extends Component {
+  render() {
+    return <div>This is going to be a POI</div>;
+  }
+}
---
+++ src/views/AllDisplay.js
@@ -0,0 +1,1 @@
+import POIDisplay from "../components/POIDisplay";
