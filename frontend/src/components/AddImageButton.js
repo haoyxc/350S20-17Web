9a3ce52c900320a1f216a7636917e65ddbc55068
mgarciaferreiro <martagarciaferreiro@gmail.com>
2020-04-16 17:39:35 +0200

    adding images from filesystem

---
+++ frontend/src/components/AddImageButton.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faImage } from '@fortawesome/free-solid-svg-icons'
+
+export default props => 
+    <div className='button fadein'>
+      <label htmlFor='single'>
+        <FontAwesomeIcon icon={faImage} color='#3B5998' size='5x' />
+      </label>
+      <input type='file' id='single' onChange={props.onChange} /> 
+    </div>
